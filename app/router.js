d3d156c9454b50097915e21175896d707318364c
yanyongchao <yongchao.yan@welian.com>
2018-06-26 23:46:48 +0800

    post

--- app/router.js
+++ app/router.js
@@ -8,7 +8,7 @@ module.exports = app => {
   router.post('/api/user/signup', controller.user.signup)
   router.post('/api/user/signin', controller.user.signin)
   router.get('/api/user/signout', controller.user.signout)
-  router.get('/api/user/access', controller.user.access)
+  router.post('/api/user/access', controller.user.access)
   router.get('/api/user/pvs', controller.user.getPvs)
   router.resources('category', '/api/category', controller.category)
   router.resources('classification', '/api/classification', controller.classification)
